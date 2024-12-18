758d7f5f39a3782f0c956b30107a5f9c40b7f978
Florid6666 <pratyushkumarsethi37@gmail.com>
2024-12-18 23:45:28 +0530

    First commit

--- get.js
+++ get.js
@@ -1,5 +1,5 @@
 import handler from "./libs/handler-lib";
-import dynamoDb from "./libs/libs/dynamodb-lib";
+import dynamoDb from "./libs/dynamodb-lib";
 export const main = handler(async (event, context) => {
 const params = {
 TableName: process.env.tableName,
